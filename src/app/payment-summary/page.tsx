3a227fb0d19297787fa66b8f9b22f4ac3cf9e01c
MichaelNegrete16 <michael.negrete16@gmail.com>
2025-07-22 16:55:21 -0500

    fix(payment): wrapped in a suspense boundary

--- src/app/payment-summary/page.tsx
+++ src/app/payment-summary/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import ResumenPayment from "@/components/Resumen";
+import { Suspense } from "react";
 
 const PaymentSummaryPage = () => {
   return (
     <div style={{ minHeight: "100vh", backgroundColor: "#f8fafc" }}>
-      <ResumenPayment />
+      <Suspense fallback={<div>Cargando resumen...</div>}>
+        <ResumenPayment />
+      </Suspense>
     </div>
   );
 };
